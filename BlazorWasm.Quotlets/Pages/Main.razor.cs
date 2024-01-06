e4f0736ca07eac28b6da0b7eb0ebd5f5880acc88
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-01-06 23:32:47 +0630

    -

--- BlazorWasm.Quotlets/Pages/Main.razor.cs
+++ BlazorWasm.Quotlets/Pages/Main.razor.cs
@@ -4,18 +4,19 @@ namespace BlazorWasm.Quotlets.Pages
 {
     public partial class Main
     {
-        //protected override async Task OnAfterRenderAsync(bool firstRender)
-        //{
-        //    if (firstRender)
-        //    {
-        //        await LoadJavaScript();
-        //    }
-        //}
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await LoadJavaScript();
+            }
+        }
 
-        //private async Task LoadJavaScript()
-        //{
-        //    await Task.Delay(1000);
-        //    await JsRuntime.InvokeVoidAsync("loadJs", "wp-content/themes/serano/js/scripts.js");
-        //}
+        private async Task LoadJavaScript()
+        {
+            await Task.Delay(1000);
+            await JsRuntime.InvokeVoidAsync("loadJs", "assets/js/switch.js");
+            await JsRuntime.InvokeVoidAsync("loadJs", "assets/js/main.js");
+        }
     }
 }
